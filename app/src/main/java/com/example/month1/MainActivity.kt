c18b85ad9184e0c41a669e9e31c09efa6ac224bc
Hansel Omondi <ogopabhs@gmail.com>
2024-04-19 23:38:35 +0300

    Add: Resources required for the project

---
+++ app/src/main/java/com/example/month1/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.month1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.month1.ui.Month1App
+import com.example.month1.ui.theme.Month1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Month1Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Month1App()
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Month1Theme {
+        Month1App()
+    }
+}
